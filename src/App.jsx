1b9338c703a30b1729bdca0a0755cdf3ae88b6f2
hututudor <hutu.tudor@yahoo.com>
2019-03-18 10:51:39 +0200

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="header">
+          <div>
+            <h1>Quantum</h1>
+            <h2>If you are coachless, just buy a coach</h2>
+          </div>
+        </div>
+        <div className="about">
+          <h1>About us</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae omnis
+            dolorem rerum vitae tempora, est nam sunt incidunt earum velit? Quod
+            et numquam voluptates omnis sunt ad esse doloremque eveniet? Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+            minus, ipsa nostrum fuga accusamus corrupti labore nulla doloribus
+            itaque? Est aut illo vero aspernatur sed minima ut sunt voluptatum
+            facere. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Omnis excepturi minima fugit recusandae aliquam rerum iusto nihil
+            temporibus, dicta repellat rem placeat laudantium officiis. Omnis
+            temporibus iusto ipsum. Incidunt, nemo! Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Ipsa perspiciatis quos fugiat
+            voluptatem aliquid, sint accusamus voluptate vero expedita
+            exercitationem laboriosam doloribus dolorum ad reiciendis tempora!
+            Quaerat animi error excepturi?
+          </p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
